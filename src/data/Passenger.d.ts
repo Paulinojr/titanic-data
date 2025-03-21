42fe16adefac959447859284ca7dc57d53d2cf1c
Paulinojr <paulino.junior@outlook.com>
2025-03-21 17:09:04 -0300

    Feat: add Passenger data type and mocked object

---
+++ src/data/Passenger.d.ts
@@ -0,0 +1,14 @@
+export type Passenger = {
+  passengerId: number;
+  survived: number;
+  pclass: number;
+  name: string;
+  sex: string;
+  age: string;
+  sibSp: number;
+  parch: number;
+  ticket: number;
+  fare: number;
+  cabin: string;
+  embarked: string;
+};
